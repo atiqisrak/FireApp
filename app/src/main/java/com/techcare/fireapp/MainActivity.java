908ff170f9bf88bdf6651ac85ec32ab504e7e570
Niloy Niil <blueniil9@gmail.com>
2019-07-29 00:02:03 +0600

    2nd

---
+++ app/src/main/java/com/techcare/fireapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.techcare.fireapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.iid.FirebaseInstanceIdReceiver;
+
+public class MainActivity extends AppCompatActivity {
+
+//    private Button mSendData;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+ //       mSendData = (Button) findViewById(R.id.addString);
+
+    }
+}
---
+++ app/src/main/java/com/techcare/fireapp/shades.java
@@ -0,0 +1,14 @@
+package com.techcare.fireapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class shades extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shades);
+    }
+}
